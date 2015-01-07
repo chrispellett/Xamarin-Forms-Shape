74705ab61ea79d271944d44fd319eb3594efa4e0
Chris Pellett <chris.pellett@gmail.com>
2015-01-07 12:41:34 +0000

    Adding XAML sample

--- DrawShape/App.cs
+++ DrawShape/App.cs
@@ -7,33 +7,37 @@ namespace DrawShape
 	{
 		public static Page GetMainPage ()
 		{	
-			return new ContentPage () {
-				Content = new StackLayout () {
-					Children = {
-						new Label () {
-							Text = "Hello world"
-						},
-						new BoxView () {
-							Color = Color.Red
-						},
-						new ShapeView () {
-							ShapeType = ShapeType.Circle,
-							StrokeColor = Color.Blue,
-							Color = Color.Yellow,
-							StrokeWidth = 5f
-						},
-						new ShapeView(){
-							ShapeType = ShapeType.CircleIndicator,
-							StrokeColor = Color.Silver,
-							IndicatorPercentage = 75f,
-							StrokeWidth = 3f
- 						}
-					},
-					Orientation = StackOrientation.Horizontal,
-					HorizontalOptions = LayoutOptions.FillAndExpand,
-					VerticalOptions = LayoutOptions.Center,
-				}
-			};
+			// XAML sample
+			return new SamplePage ();
+
+			// Code sample
+//			return new ContentPage () {
+//				Content = new StackLayout () {
+//					Children = {
+//						new Label () {
+//							Text = "Hello world"
+//						},
+//						new BoxView () {
+//							Color = Color.Red
+//						},
+//						new ShapeView () {
+//							ShapeType = ShapeType.Circle,
+//							StrokeColor = Color.Blue,
+//							Color = Color.Yellow,
+//							StrokeWidth = 5f
+//						},
+//						new ShapeView(){
+//							ShapeType = ShapeType.CircleIndicator,
+//							StrokeColor = Color.Silver,
+//							IndicatorPercentage = 75f,
+//							StrokeWidth = 3f
+// 						}
+//					},
+//					Orientation = StackOrientation.Horizontal,
+//					HorizontalOptions = LayoutOptions.FillAndExpand,
+//					VerticalOptions = LayoutOptions.Center,
+//				}
+//			};
 		}
 	}
 }
