cccf370f5ddff107eb1319b544cb67b6057188d0
Benjamin <benjamin.laborier@metsia.fr>
2017-02-08 17:11:35 +0100

    Add windows phone support for box and circle (circle indicator not supported yet)

---
+++ DrawShape.WinPhone/MainPage.xaml.cs
@@ -0,0 +1,36 @@
+﻿using Windows.UI.Xaml.Navigation;
+
+// Pour en savoir plus sur le modèle d'élément Page vierge, consultez la page http://go.microsoft.com/fwlink/?LinkId=391641
+
+namespace DrawShape.WinPhone
+{
+    /// <summary>
+    /// Une page vide peut être utilisée seule ou constituer une page de destination au sein d'un frame.
+    /// </summary>
+    public sealed partial class MainPage
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+
+            LoadApplication(new DrawShape.App());
+            this.NavigationCacheMode = NavigationCacheMode.Required;
+        }
+
+        /// <summary>
+        /// Invoqué lorsque cette page est sur le point d'être affichée dans un frame.
+        /// </summary>
+        /// <param name="e">Données d'événement décrivant la manière dont l'utilisateur a accédé à cette page.
+        /// Ce paramètre est généralement utilisé pour configurer la page.</param>
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            // TODO: préparer la page pour affichage ici.
+
+            // TODO: si votre application comporte plusieurs pages, assurez-vous que vous
+            // gérez le bouton Retour physique en vous inscrivant à l’événement
+            // Événement Windows.Phone.UI.Input.HardwareButtons.BackPressed.
+            // Si vous utilisez le NavigationHelper fourni par certains modèles,
+            // cet événement est géré automatiquement.
+        }
+    }
+}
---
+++ DrawShape.WinPhone/ShapeRenderer.cs
@@ -0,0 +1,60 @@
+﻿using DrawShape;
+using DrawShape.WinPhone;
+using Xamarin.Forms.Platform.WinRT;
+
+[assembly:ExportRenderer (typeof(ShapeView), typeof(ShapeRenderer))]
+namespace DrawShape.WinPhone
+{
+    using Windows.UI.Xaml;
+    using Windows.UI.Xaml.Media;
+    using Windows.UI.Xaml.Shapes;
+    using DrawShape;
+    using Xamarin.Forms.Platform.WinRT;
+
+    public class ShapeRenderer : ViewRenderer<ShapeView, Shape>
+    {
+        private Ellipse _ellipse;
+        private Rectangle _rectangle;
+
+        protected override void OnElementChanged(ElementChangedEventArgs<ShapeView> e)
+        {
+            base.OnElementChanged(e);
+
+            if (e.OldElement != null || Element == null)
+                return;
+
+            var converter = new ColorConverter();
+
+            var color = (SolidColorBrush)converter.Convert(Element.Color, null, null, null);
+            var strockColor = (SolidColorBrush)converter.Convert(Element.StrokeColor, null, null, null);
+            switch (Element.ShapeType)
+            {
+                case ShapeType.Box:
+                    _rectangle = new Rectangle
+                    {
+                        Fill = color,
+                        Stroke = strockColor,
+                        StrokeThickness = Element.StrokeWidth,
+                        Margin = new Thickness(Element.Padding.HorizontalThickness),
+                        RadiusX = Element.CornerRadius,
+                        RadiusY = Element.CornerRadius,
+                    };
+                    SetNativeControl(_rectangle);
+                    break;
+                case ShapeType.Circle:
+                    _ellipse = new Ellipse
+                    {
+                        Fill = color,
+                        Stroke = strockColor,
+                        StrokeThickness = Element.StrokeWidth,
+                        Margin = new Thickness(Element.Padding.HorizontalThickness)
+                    };
+                    SetNativeControl(_ellipse);
+                    break;
+                case ShapeType.CircleIndicator:
+                    // TODO WinPhone circle indicator
+                    break;
+            }
+        }
+    }
+}
